fbbe796da835fc9f9cd57e20a662aae4abd4c82f
Kathryn Schroeder <kthrnschrdr@gmail.com>
2014-09-04 15:29:49 -0500

    j-Querified!

--- ttt.js
+++ ttt.js
@@ -1,61 +1,58 @@
-window.onload = function(){
+$(document).ready(function(){
   
   // Collection of all squares on the board.
-  var squares = document.getElementsByTagName('td');
+  var $squares = $('td');
   
   var timer = 0;
   var turn = "";
-  var catsTimer = 0;
   
   var add_mark = function(){
-    if(timer % 2 == 0 && this.innerHTML == "-"){
+    if(timer % 2 == 0){ // && this.innerHTML == "-"){
       var turn = "X";
       this.innerHTML = turn;
       timer++;
-      catsTimer++;
-    }else if(timer % 2 == 1 && this.innerHTML == "-"){
+    }else if(timer % 2 == 1){ // && this.innerHTML == "-"
       var turn = "O";
       this.innerHTML = turn;
       timer++;
-      catsTimer++;
     }
     win();
   }
-  var i = 0;
+  // var i = 0;
   
  function win() {
     if(
-      (squares[0].innerHTML == "X" && squares[1].innerHTML == "X" && squares[2].innerHTML == "X") ||
-      (squares[3].innerHTML == "X" && squares[4].innerHTML == "X" && squares[5].innerHTML == "X") ||
-      (squares[6].innerHTML == "X" && squares[7].innerHTML == "X" && squares[8].innerHTML == "X") ||
-      (squares[0].innerHTML == "X" && squares[3].innerHTML == "X" && squares[6].innerHTML == "X") ||
-      (squares[1].innerHTML == "X" && squares[4].innerHTML == "X" && squares[7].innerHTML == "X") ||
-      (squares[2].innerHTML == "X" && squares[5].innerHTML == "X" && squares[8].innerHTML == "X") ||
-      (squares[0].innerHTML == "X" && squares[4].innerHTML == "X" && squares[8].innerHTML == "X") ||
-      (squares[2].innerHTML == "X" && squares[4].innerHTML == "X" && squares[6].innerHTML == "X")) {
+      ($squares[0].innerHTML == "X" && $squares[1].innerHTML == "X" && $squares[2].innerHTML == "X") ||
+      ($squares[3].innerHTML == "X" && $squares[4].innerHTML == "X" && $squares[5].innerHTML == "X") ||
+      ($squares[6].innerHTML == "X" && $squares[7].innerHTML == "X" && $squares[8].innerHTML == "X") ||
+      ($squares[0].innerHTML == "X" && $squares[3].innerHTML == "X" && $squares[6].innerHTML == "X") ||
+      ($squares[1].innerHTML == "X" && $squares[4].innerHTML == "X" && $squares[7].innerHTML == "X") ||
+      ($squares[2].innerHTML == "X" && $squares[5].innerHTML == "X" && $squares[8].innerHTML == "X") ||
+      ($squares[0].innerHTML == "X" && $squares[4].innerHTML == "X" && $squares[8].innerHTML == "X") ||
+      ($squares[2].innerHTML == "X" && $squares[4].innerHTML == "X" && $squares[6].innerHTML == "X")) {
       console.log("We have a winner!");
-      i = 10;
+      // i = 10;
     } else if(
-      (squares[0].innerHTML == "O" && squares[1].innerHTML == "O" && squares[2].innerHTML == "O") ||
-      (squares[3].innerHTML == "O" && squares[4].innerHTML == "O" && squares[5].innerHTML == "O") ||
-      (squares[6].innerHTML == "O" && squares[7].innerHTML == "O" && squares[8].innerHTML == "O") ||
-      (squares[0].innerHTML == "O" && squares[3].innerHTML == "O" && squares[6].innerHTML == "O") ||
-      (squares[1].innerHTML == "O" && squares[4].innerHTML == "O" && squares[7].innerHTML == "O") ||
-      (squares[2].innerHTML == "O" && squares[5].innerHTML == "O" && squares[8].innerHTML == "O") ||
-      (squares[0].innerHTML == "O" && squares[4].innerHTML == "O" && squares[8].innerHTML == "O") ||
-      (squares[2].innerHTML == "O" && squares[4].innerHTML == "O" && squares[6].innerHTML == "O")) {
+      ($squares[0].innerHTML == "O" && $squares[1].innerHTML == "O" && $squares[2].innerHTML == "O") ||
+      ($squares[3].innerHTML == "O" && $squares[4].innerHTML == "O" && $squares[5].innerHTML == "O") ||
+      ($squares[6].innerHTML == "O" && $squares[7].innerHTML == "O" && $squares[8].innerHTML == "O") ||
+      ($squares[0].innerHTML == "O" && $squares[3].innerHTML == "O" && $squares[6].innerHTML == "O") ||
+      ($squares[1].innerHTML == "O" && $squares[4].innerHTML == "O" && $squares[7].innerHTML == "O") ||
+      ($squares[2].innerHTML == "O" && $squares[5].innerHTML == "O" && $squares[8].innerHTML == "O") ||
+      ($squares[0].innerHTML == "O" && $squares[4].innerHTML == "O" && $squares[8].innerHTML == "O") ||
+      ($squares[2].innerHTML == "O" && $squares[4].innerHTML == "O" && $squares[6].innerHTML == "O")) {
       console.log("We have a winner!");
-      i = 10
+      // i = 10
     } else {
-        if(catsTimer == 9){  
+        if(timer == 9){  
           console.log("Cat's Game");
         }
     }
   }
   
-  for(i = 0; i < squares.length; i++) {
-    // When you click a square, runs the `add_mark` method.
-    squares[i].addEventListener("click", add_mark);
-  }
+   $squares.each(function(i){
+	   $(this).on("click", add_mark);
+	  });
+  
 
-}
+});
